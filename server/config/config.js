d97aced15af70139094d3bcb102f253bf0802fb0
jcrf15 <59666296+jcrf15@users.noreply.github.com>
2020-09-15 22:26:59 -0700

    RestServer - usaurio

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,32 @@
 //=================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//===========
+//Entorno
+//==========
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+
+//===========
+//Base de datos
+//==========
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+
+} else {
+
+    urlDB = 'mongodb+srv://jcrf15:Jcrf151681.@cluster0.yn9pz.mongodb.net/cafe?retryWrites=true&w=majority'
+}
+
+process.env.URLDB = urlDB;
+// mongodb + srv: //jcrf15:<password>@cluster0.yn9pz.mongodb.net/test
+
+
+// mon
---
+++ server/models/usuario.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+};
+let usuarioSchema = new Schema({
+
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+
+    },
+    email: {
+        type: String,
+        require: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio ']
+    },
+
+    img: {
+        type: String,
+        required: [false]
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+
+    }
+
+
+
+
+
+});
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+}
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
